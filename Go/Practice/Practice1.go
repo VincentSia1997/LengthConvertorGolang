d7b48d72cfd79b6db38983bb31c9d4bfc3da5ffe
VincentSiaYuXing <vincentyuxing.sia@my.jcu.edu.au>
2024-12-17 21:05:24 +0800

    Test sync

---
+++ Go/Practice/Practice1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	PI = 3.14
+)
+
+func main() {
+	// var student1 string = "Ali"
+	// var student3 = "baba"
+	// x := 3
+
+	// var myNum = 58
+
+	var a, b, c, d, e, f, g int = 1, 3, 5, 7, 9, 11, 13
+
+	fmt.Println("Hello World!")
+	// fmt.Println(student1)
+	// fmt.Println(student3)
+	// fmt.Println(x)
+	// fmt.Println(myNum)
+
+	fmt.Println(a + b)
+	fmt.Println(c - d)
+	fmt.Println(float64(e * f / g))
+}
